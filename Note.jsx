66a62b5e41a3aea99ec7a3b2f6e26367941d001a
Yashi002 <50695892+Yashi002@users.noreply.github.com>
2021-06-05 19:47:46 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcam by Shaurya Sinha. It was an awesome
+        experience.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
